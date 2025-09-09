33e9c7139bdb2a3b83432319e79df429bed19d6f
shubham7007 <raishubham999.sr@gmail.com>
2025-09-09 17:24:35 +0530

    updated with interactive options for engine transmission fuel type brand power seats

--- views/car_price_prediction.py
+++ views/car_price_prediction.py
@@ -8,6 +8,29 @@ st.title('car price prediction app')
 st.write('data from cardekho web scraping by github/krishnaik')
 
 
+#Encode Standardize Predict
+#load encoders and model
+with open('views/label_encoders.pkl', 'rb') as f:
+    label_encoders = pickle.load(f)
+
+with open('views/scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+with open('views/rf_model.pkl', 'rb') as f:
+    car_price_model = pickle.load(f)
+
+with open('views/car_lookup.pkl', 'rb') as f:
+    car_lookup = pickle.load(f)
+
+
+#adding 'Brand' NON functional for ML
+brand = st.selectbox("Select Car Brand", sorted(car_lookup.keys()))
+
+
+available_models = sorted(car_lookup[brand].keys())
+model = st.selectbox('Model', available_models)
+ 
+
 
 
 feature_order = [
@@ -16,45 +39,34 @@ feature_order = [
 
 #scrollable Inputs for Categorical Features
 seller_type = st.selectbox('Seller Type',['Dealer','Individual','Trustmark Dealer'])
-fuel_type = st.selectbox('Fuel Type',['Petrol','Diesel','CNG','LPG','Electric'])
-transmission_type = st.selectbox('Transmission Type',['Manual','Automatic'])
-model = st.selectbox('Model',['i20','Swift Dzire','Swift','Alto','City','Wagon R','Grand','Innova',
-                              'Verna','i10','Ecosport','Polo','Baleno','Amaze','Ciaz','Eritga',
-                              'Creta','XUV500','KWID','Vitara','Scorpio','Figo','Vento','Celerio',
-                              'Duster','Bolero','Fortuner','Rapid','Jazz','3','Tiago','Santro',
-                              'E-Class','Eeco','C-Class','5','WR-V','Safari','A4','Superb','Nexon',
-                              'Go','RediGo','Ignis','KUV','ASpire','A6','Thar','Octavia','Civic',
-                              'Venue','X1','XF','Rover','Elantra','Endeavour','Hexa','Compass','7',
-                              'S-Class','Tigor','GL-Class','FreeStyle','CR-V','Seltos',
-                              'KUV100','X5','Marazzo','X3','Q7','Harrier','Hector','6','Yaris',
-                              'Cooper','Dzire VXI','Cayenne','XUV300','S-Presso','GLS','Triber',
-                              'Tucson','redi-Go','CLS','Kicks','Glanza','XL6','D-max','XC','Z4',
-                              'X4','A8','ES','XC60','S90','XE','Carnival','XC90','Dzire ZXI',
-                              'Continental','CR','Panamera','F-Pace','Alturas','X-Trail','MUX',
-                              'Wrangler','RX','NX','Dzire LXI','Macan','Ghibli','Aura','GTC4Lusso',
-                              'Altroz','C','Ghost','Quattroporte','Gurkha'])
- 
+
+fuel_options = sorted(car_lookup[brand][model]['fuel_type'])
+fuel_type = st.selectbox('Fuel Type',fuel_options)
+
+transmission_options = sorted(car_lookup[brand][model]['transmission_type'])
+transmission_type = st.selectbox('Transmission Type', transmission_options)
+
+
 
 #Numerical Inputs
 vehicle_age = st.number_input('Vehicle Age', min_value=0.0)
 km_driven = st.number_input('Kms Driven', min_value=0.0)
 mileage = st.number_input('Mileage', min_value=0.0)
-engine = st.number_input('Engine Capacity CC', min_value=0.0)
-max_power =st.number_input('max_power BHP', min_value=0.0)
-seats = st.number_input('Seats', min_value=0.0)
+
+engine_options = sorted(car_lookup[brand][model]['engine'])
+engine = st.selectbox('Engine-CC', engine_options)
+
+power_options = sorted(car_lookup[brand][model]['max_power']) 
+max_power = st.selectbox('Power-BHP',power_options )
+
+seat_options = sorted(car_lookup[brand][model]['seats'])
+seats = st.selectbox('No. of Seats',seat_options)
+
+
+
+
 
         
-
-#Encode Standardize Predict
-#load encoders and model
-with open('models/label_encoders.pkl', 'rb') as f:
-    label_encoders = pickle.load(f)
-
-with open('models/scaler.pkl', 'rb') as f:
-    scaler = pickle.load(f)
-
-with open('models/rf_model.pkl', 'rb') as f:
-    car_price_model = pickle.load(f)
 
 
 
